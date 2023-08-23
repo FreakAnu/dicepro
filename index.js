1b8a6417f8895e831a007d0cdcd2b820f7bfb737
FreakAnu <141220434+FreakAnu@users.noreply.github.com>
2023-08-23 13:33:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 var randomNumber1 = Math.floor(Math.random() * 6) + 1;
 var randomdiceimage = "dice" + randomNumber1 + ".png";
-var diceimagesource = "images/" + randomdiceimage;
+var diceimagesource = "./images/" + randomdiceimage;
 var image1 =document.querySelectorAll(".img1")[0];
 image1.setAttribute("src",diceimagesource);
 
 var randomNumber2 = Math.floor(Math.random() * 6) + 1;
 var randomdiceimage2 = "dice" + randomNumber2 + ".png";
-var diceimagesource = "images/" + randomdiceimage2;
+var diceimagesource = "./images/" + randomdiceimage2;
 var image2 =document.querySelectorAll(".img2")[0];
 image2.setAttribute("src",diceimagesource);
 
