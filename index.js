a1918d3477986d94ac89627651988b1266d87889
FreakAnu <141220434+FreakAnu@users.noreply.github.com>
2023-08-23 13:23:53 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+var randomdiceimage = "dice" + randomNumber1 + ".png";
+var diceimagesource = "images/" + randomdiceimage;
+var image1 =document.querySelectorAll(".img1")[0];
+image1.setAttribute("src",diceimagesource);
+
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+var randomdiceimage2 = "dice" + randomNumber2 + ".png";
+var diceimagesource = "images/" + randomdiceimage2;
+var image2 =document.querySelectorAll(".img2")[0];
+image2.setAttribute("src",diceimagesource);
+
+if(randomNumber1 > randomNumber2){
+    document.querySelector("h1").innerHTML=" Player 1 Won";
+}
+else if(randomNumber1 < randomNumber2){
+    document.querySelector("h1").innerHTML=" Player 2 Won";
+}
+else{
+    document.querySelector("h1").innerHTML=" Draw";
+}
